851c65d554eb763f19b766eae69d20d7d9847da7
Muhammad Mohsin <62098725+Zinbadgg@users.noreply.github.com>
2023-01-23 14:29:11 +0000

    Update regressionModel.m

--- CarPricePrediction/regressionModel.m
+++ CarPricePrediction/regressionModel.m
@@ -4,4 +4,12 @@
 y = table2array(carDataFinal(:,"price"));
 x = table2array(carDataFinal(:,"mileage"));
 
-b1 = x\y
+b1 = x\y;
+yCalc1 = b1*x;
+scatter(x,y)
+hold on
+plot(x,yCalc1)
+xlabel('Milage')
+ylabel('Price')
+title('Linear Regression Relation Between Milage & Price')
+grid on
