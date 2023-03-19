15c54c9b3be6d0b35237fbf95456c76cb4528b3d
Muhammad Mohsin <62098725+MM0hsin@users.noreply.github.com>
2023-03-19 18:18:36 +0000

    Update

--- CarPricePrediction/100kCars/carData.m
+++ CarPricePrediction/100kCars/carData.m
@@ -4,33 +4,30 @@ modelType = table2array(x(:,"model"));
 transmissionType = table2array(x(:,"transmission"));
 fuelType = table2array(x(:,"fuelType"));
 
-[name,~,nameNarray] = unique(modelType);
-nameN = 1:size(name);
-typeModel = dictionary(name,nameN.');
-
-
 [transmission,~,transmissionNarray] = unique(transmissionType);
 transmissionN = 1:size(transmission);
 typeTransmission = dictionary(transmission,transmissionN.');
-
 
 [fuel,~,fuelNarray] = unique(fuelType);
 fuelN = 1:size(fuel);
 typeFuel = dictionary(fuel,fuelN.');
 
 carDataFinal = x(:,:);
-carDataFinal.model = nameNarray;
 
 carDataFinal.transmission = transmissionNarray;
 manual = [];
 automatic = [];
 for i=1:height(transmissionNarray)
-    if transmissionNarray(i,1) == 1
+    if transmissionNarray(i,1) == 2
         manual(i) = 1;
         automatic(i) = 0;
-    else
+    elseif transmissionNarray(i,1) == 1 || transmissionNarray(i,1) == 4
         automatic(i) = 1;
         manual(i) = 0;
+    else
+        automatic(i) = 9;
+        manual(i) = 9;
+
     end
 end
 
@@ -52,21 +49,28 @@ for i=1:height(fuelNarray)
         hybrid(i) = 1;
         diesel(i) = 0;
         petrol(i) = 0;
-    else 
+    elseif fuelNarray(i) == 4
          petrol(i) = 1;
          diesel(i) = 0;
          hybrid(i) = 0;
+    else
+        petrol(i) = 9;
+        diesel(i) = 9;
+        hybrid(i) = 9;
     end
 end
 carDataFinal.petrol = petrol.';
 carDataFinal.diesel = diesel.';
 carDataFinal.hybrid = hybrid.';
 
-%% Removing outliers:
+%Remove 'other' fueltype
+FT9 = carDataFinal.petrol ==9;
+carDataFinal(FT9,:) = [];
 
-% mileageOutlier = carDataFinal.mileage > 66000;
-% carDataFinal.mileage(mileageOutlier) = 0;
-% carDataFinal(~carDataFinal.mileage,:) = [];
+%Remove 'other' transmission
+T9 = carDataFinal.automatic == 9;
+carDataFinal(T9,:) = [];
+
 
 
 
